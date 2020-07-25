986decf1bf6f27fb243de0e535919a14370c87d7
Agus Cahyono <aguscahyono@roomme.space>
2020-07-25 17:27:23 +0700

    Initial Commit

---
+++ OnBoardingUI/OnBoardingUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnBoardingUIApp.swift
+//  OnBoardingUI
+//
+//  Created by Agus RoomMe on 25/07/20.
+//
+
+import SwiftUI
+
+@main
+struct OnBoardingUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
